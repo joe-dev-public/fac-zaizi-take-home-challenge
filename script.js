2f22e2048f4dc9605fc1400d5bb7a3923113cf77
joe <joe.dev.public@gmail.com>
2022-03-24 20:30:15 +0000

    Clean up useful comments

--- script.js
+++ script.js
@@ -2,31 +2,51 @@
 
 /*
 
-    Possible approach:
-
-        - Generate 1,000 (more? less?) pseudo-random (PR) numbers between 1 and 3, and store them in an array.
-        - Show the user (graphically) that the distribution is "approximately uniform".
-            (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
-        - These PRNs determine which door the player picks, for a number of game simulations.
-        - Do different sets of simulations (each time using the entire PRN array).
-        - The winning door (car) will naturally be set at the start of each set.
-        - You can have 3 different winning positions.
-        - You can have 2 different approaches (stick, switch).
-        - So you could do at least 6 simulations?
+    Approach:
+    ---------
+        - Generate two sets of 10-1000 pseudo-random (PR) numbers, 1-3 inclusive.
+        - Store them in an array.
+        - These PRN sets determine:
+            - Which door the player initially picks.
+            - Which is the winning door.
+        - Run a simulation using the full PRN arrays.
         - Show a graphical representation of the simulation (number of wins for that strategy).
 
 
-    Notes:
+    Stretch goals?
+    --------------
+        - Reinforce the result with multiple sets:
+            - Do multiple sets of 10-1000 simulations.
+            - Store the results each time.
+            - Calculate the ratio switch wins/stick wins for each run of simulations.
+            - Show the user a scatter plot of these to reinforce that the switch strategy is
+                roughly 2x more successful than the stick strategy.
 
-        - Do we actually need to work with the same set of PRNs?
-        - Are we doing that to reassure the user about randommness?
-        - Will it actually reassure them? :)
+        - Reassure the user about the nature of the PRNs used:
+            - Show the user (bar graph?) that the distribution of PRNs used is "approx. uniform".
+                (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+
+
+    A breakdown of the probabilities/solution for my own sake :)
+    ------------------------------------------------------------
+        - At the start of the game, you've got:
+            - 1/3 chance of picking the car, and
+            - 2/3 chance of picking a goat.
+        - If you pick a goat (2/3) and switch, you're guaranteed to win.
+        - If you pick the car (1/3) and switch, you're guaranteed to lose.
+        - (So if you use the switch strategy, you'll win 2/3 of the time.)
+        - If you pick a goat (2/3) and stick, you're guaranteed to lose.
+        - If you pick the car (1/3) and stick, you're guaranteed to win.
+        - (So if you use the stick strategy, you'll win 1/3 of the time.)
 
 */
 
 
 
 function drawChart(stickData, switchData) {
+
+    // Scatter plot game results and a trend line should emerge to the viewer.
+    // x-axis: number of games played. y-axis: number of wins (cumulative)
 
     const myLabels = [];
     for (let i = 1; i <= numberOfGamesToSimulate; i++) {
@@ -191,144 +211,6 @@ function getRandomInt(min, max) {
 
 
 
-function runSimulationsA() {
-
-    const pseudoRandomNumbers = [];
-
-    for (let i = 0; i < numberOfGamesToSimulate; i++) {
-        pseudoRandomNumbers.push(getRandomInt(1, 4));
-    }
-
-
-    // My game-playing loops effectively hard-code the probabilities.
-    // Is this a good way of convincing an observer?
-    // Maybe not, it feels like it's a logical loop!
-    // Does it make more sense to use two different groups for the stick vs stay games?
-    // (Or even a different group for every game?)
-
-
-    // Games 1-3: stick
-
-    // First door wins, second door wins, etc.:
-    const game1Setup = [1, 0, 0];
-    const game2Setup = [0, 1, 0];
-    const game3Setup = [0, 0, 1];
-
-    // Games 4-6: switch
-    const game4Setup = [1, 0, 0];
-    const game5Setup = [0, 1, 0];
-    const game6Setup = [0, 0, 1];
-
-
-    const stickSetup1Results = [];
-    const stickSetup2Results = [];
-    const stickSetup3Results = [];
-    const switchSetup1Results = [];
-    const switchSetup2Results = [];
-    const switchSetup3Results = [];
-
-
-    pseudoRandomNumbers.forEach((playerDoorSelection, index) => {
-
-        const selectedDoor = playerDoorSelection - 1;
-
-        if (game1Setup[selectedDoor] === 1) {
-            // The player's initial guess was correct.
-            // This is the only case in which the "stick" strategy leads to a win.
-            // So, no other branch needs to be handled, we can just tally the win.
-            stickSetup1Results.push(true);
-        } else {
-            stickSetup1Results.push(false);
-        }
-
-        // The two other games naturally play out the same way:
-        if (game2Setup[selectedDoor] === 1) {
-            stickSetup2Results.push(true);
-        } else {
-            stickSetup2Results.push(false);
-        }
-        if (game3Setup[selectedDoor] === 1) {
-            stickSetup3Results.push(true);
-        } else {
-            stickSetup3Results.push(false);
-        }
-
-
-        if (game4Setup[selectedDoor] === 1) {
-            // The player's initial guess was correct.
-            // This is the only case in which the "switch" strategy leads to a loss.
-            // (Do nothing.)
-            switchSetup1Results.push(false);
-        } else {
-            // The player's initial guess was incorrect. A losing door is opened.
-            // The remaining (unopened) door is the winning one.
-            // As the switch strategy is being used, the player now wins.
-            switchSetup1Results.push(true);
-        }
-
-        /*  In other words: at the start of the game, you've got 1/3 chance of picking the car,
-            and 2/3 chance of picking a goat.
-            If you pick a goat and switch, you're guaranteed to win.
-            If you pick the car and switch, you're guaranteed to lose.
-            (This is approaching a complete listing of the sample space, but neverthless
-            it feels like a neat way to explain the logic of the solution :)
-
-            If you pick a goat and stick, you're guaranteed to lose.
-            If you pick the car and stick, you're guaranteed to win.
-        */
-
-        if (game5Setup[selectedDoor] === 0) {
-            switchSetup2Results.push(true);
-        } else {
-            switchSetup2Results.push(false);
-        }
-
-        if (game6Setup[selectedDoor] === 0) {
-            switchSetup3Results.push(true);
-        } else {
-            switchSetup3Results.push(false);
-        }
-
-    });
-
-
-    // Scatter plot game results and a trend line will naturally emerge.
-    // x-axis: number of games played. y-axis: number of wins (cumulative)
-
-    const stickSetup1CumulativeResults = [];
-    const switchSetup1CumulativeResults = [];
-
-    for (const result of stickSetup1Results) {
-        const previousVal = stickSetup1CumulativeResults[stickSetup1CumulativeResults.length - 1] || 0;
-        if (result === true) {
-            stickSetup1CumulativeResults.push(previousVal + 1);
-        } else {
-            stickSetup1CumulativeResults.push(previousVal);
-        }
-    }
-
-    for (const result of switchSetup1Results) {
-        const previousVal = switchSetup1CumulativeResults[switchSetup1CumulativeResults.length - 1] || 0;
-        if (result === true) {
-            switchSetup1CumulativeResults.push(previousVal + 1);
-        } else {
-            switchSetup1CumulativeResults.push(previousVal);
-        }
-    }
-
-
-    // OK, that's a reasonable first pass "proof".
-    // To be more convincing, maybe it should "play the game" more than it currently does.
-
-    drawChart(stickSetup1CumulativeResults, switchSetup1CumulativeResults);
-
-    updateConclusions(stickSetup1CumulativeResults, switchSetup1CumulativeResults);
-
-}
-// End of function runSimulationsA
-
-
-
 function runSimulationsB() {
 
     const playerInitialGuesses = [];
@@ -391,6 +273,9 @@ function runSimulationsB() {
 
     updateConclusions(stickCumulativeResults, switchCumulativeResults);
 
+    // That's a reasonable "proof".
+    // To be more convincing, could the code "play the game" more than it currently does?
+
 }
 // End of function runSimulationsB
 
@@ -418,10 +303,7 @@ formRangeEl.addEventListener('input', (event) => {
 formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     numberOfGamesToSimulate = formRangeEl.value;
-    //runSimulationsA();
     runSimulationsB();
 });
-
-//runSimulationsA();
 
 runSimulationsB();
